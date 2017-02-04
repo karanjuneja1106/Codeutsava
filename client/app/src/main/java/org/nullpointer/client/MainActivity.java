d23407ee6e51d77b2100ee40472e9944e2b9e33f
Tanishka1997 <u15co059@coed.svnit.ac.in>
2017-02-04 20:03:37 +0530

    client_medical_history

--- client/app/src/main/java/org/nullpointer/client/MainActivity.java
+++ client/app/src/main/java/org/nullpointer/client/MainActivity.java
@@ -1,8 +1,12 @@
 package org.nullpointer.client;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,8 +17,24 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    Bundle detailsImported;
+    String mailIdImportedString = "",nameImportedString = "",JSON_STRING = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +51,14 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        detailsImported=getIntent().getExtras();
+        mailIdImportedString=detailsImported.getString("mailID");
+        nameImportedString=detailsImported.getString("name");
+        getJSON();
+
+    }
+    void getJSON(){
+        new BackgroundTask(this).execute(mailIdImportedString);
     }
 
     @Override
@@ -83,10 +111,80 @@ public class MainActivity extends AppCompatActivity
         }
         else
         {
+            Intent i=new Intent(getApplicationContext(),ClientMedicalHistory.class);
+            i.putExtra("JSON_STRING",JSON_STRING);
+            startActivity(i);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    public class BackgroundTask extends AsyncTask<String,Void,String> {
+        String jsonGetMedicalHistoryURL="";
+        Context context;
+
+        BackgroundTask(Context context){
+            this.context = context;
+        }
+
+        @Override
+        protected void onPreExecute(){
+            jsonGetMedicalHistoryURL = "http://172.16.20.45/jsonGetMedicalHistory.php";
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String CLIENT_MAIL_ID=params[0];
+
+            try {
+                URL url = new URL(jsonGetMedicalHistoryURL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+
+                String data = URLEncoder.encode("CLIENT_MEDICAL_HISTORY_MAIL_ID", "UTF-8") +"="+URLEncoder.encode(CLIENT_MAIL_ID,"UTF-8");
+
+                bufferedWriter.write(data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+                StringBuilder stringBuilder = new StringBuilder();
+                while ((JSON_STRING=bufferedReader.readLine())!=null){
+                   // Log.i("fsgg",JSON_STRING);
+                    stringBuilder.append(JSON_STRING+"\n");
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return stringBuilder.toString().trim();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return "";
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... avoid){
+            super.onProgressUpdate(avoid);
+        }
+
+        @Override
+        protected void onPostExecute(String result){
+            JSON_STRING = result;
+
+        }
+    }
+
 }
