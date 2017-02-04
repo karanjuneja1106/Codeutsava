940f72264295d3f0a51e866896866ee22c221bed
Karan Juneja <karan.juneja1106@gmail.com>
2017-02-04 13:07:25 +0530

    Client class added

---
+++ client/app/src/main/java/org/nullpointer/client/Client.java
@@ -0,0 +1,57 @@
+package org.nullpointer.client;
+
+/**
+ * Created by karan1106 on 4/2/17.
+ */
+
+public class Client {
+    private String mName, mMailID, mContact, mWeeks, mCity, mState;
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public String getMailID() {
+        return mMailID;
+    }
+
+    public void setMailID(String mailID) {
+        mMailID = mailID;
+    }
+
+    public String getContact() {
+        return mContact;
+    }
+
+    public void setContact(String contact) {
+        mContact = contact;
+    }
+
+    public String getWeeks() {
+        return mWeeks;
+    }
+
+    public void setWeeks(String weeks) {
+        mWeeks = weeks;
+    }
+
+    public String getCity() {
+        return mCity;
+    }
+
+    public void setCity(String city) {
+        mCity = city;
+    }
+
+    public String getState() {
+        return mState;
+    }
+
+    public void setState(String state) {
+        mState = state;
+    }
+}
